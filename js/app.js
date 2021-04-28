391fa90f930ffedebf87c8a22c1e04a57489879a
QPThree <youngqp3@gmail.com>
2021-04-27 21:05:21 -0700

    added files from CF repo.  Fixed JS to match linter rules

--- js/app.js
+++ js/app.js
@@ -1,29 +1,30 @@
 'use strict';
 //global vars
-var username;
+let username;
+let correct;
 //questions and related answers in banks must coordinate via index
-var questionBank = [
-    'Did Quen Grow up in Maine?',
-    'Did Quen go to the University of New Hampshire?', 
-    'Is Quen planning to take the Javascript 401?', 
-    'Did Quen mention Snowboarding?', 
-    'Was Quen\'s major Kinesiology?'];
-var answerBank = [
-    'n',
-    'y',
-    'n',
-    'n',
-    'y'
-]
+let questionBank = [
+  'Did Quen Grow up in Maine?',
+  'Did Quen go to the University of New Hampshire?',
+  'Is Quen planning to take the Javascript 401?',
+  'Did Quen mention Snowboarding?',
+  'Was Quen\'s major Kinesiology?'];
+let answerBank = [
+  'n',
+  'y',
+  'n',
+  'n',
+  'y'
+];
 
 //gets username at document load
 //user must submit a valid username.  No empty strings or canceling
 function greeting(){
-    username = prompt('Hello! Please enter a username');
-    while(username == null || username == ''){
-        username = prompt('Please enter a valid username!');
-    }
-    return 'Hello ' + username + '! Welcome to my 201-Lab02 Site!';
+  username = prompt('Hello! Please enter a username');
+  while(username === null || username === ''){
+    username = prompt('Please enter a valid username!');
+  }
+  return 'Hello ' + username + '!';
 }
 
 //5 yes or no questions
@@ -32,39 +33,39 @@ function greeting(){
 //end of function will call displayResults function
 //TODO: Change console.logs to alerts for user to see
 function quizGame(){
-    correct = 0;
-    var answer;
-    for (var i = 0; i < 5; i++){
-        answer = prompt(questionBank[i]).toLowerCase();
-        while (answer != 'y' && answer != 'yes' && answer != 'no' && answer != 'n'){
-            answer = prompt('Please enter a valid respone');
-        }
-        if (answer.startsWith(answerBank[i])) {
-            correct = correct + 1;
-            console.log('Got question right');
-            //alert will go here to say they were correct
-        }
-        else{
-            console.log('Got question Wrong');
-            //alert will go here to say they were wrong
-        }
+  correct = 0;
+  let answer;
+  for (let i = 0; i < 5; i++){
+    answer = prompt(questionBank[i]).toLowerCase();
+    while (answer !== 'y' && answer !== 'yes' && answer !== 'no' && answer !== 'n'){
+      answer = prompt('Please enter a valid respone');
     }
-    if (correct > 4){
-        console.log('Smoked it!');
-    }
-    else if(correct > 2 && correct <= 4){
-        console.log('Decent job ' + username + ' You answered ' + correct + ' correctly');
+    if (answer.startsWith(answerBank[i])) {
+      correct = correct + 1;
+      console.log('Got question right');
+      alert('That\'s Correct!');
     }
     else{
-        console.log('Yikes! Were you even paying attention?');
+      console.log('Got question Wrong');
+      alert('Unfortunately, no.');
     }
-    displayResults(correct);
+  }
+  if (correct > 4){
+    console.log('Smoked it!');
+  }
+  else if(correct > 2 && correct <= 4){
+    console.log('Decent job ' + username + ' You answered ' + correct + ' correctly');
+  }
+  else{
+    console.log('Yikes! Were you even paying attention?');
+  }
+  displayResults(correct);
 }
 
 //function gets element from html and writes the users score into it with HTML
 function displayResults(correctlyAnswered){
-    e = document.getElementById("displayResults");
-    e.innerHTML = "<div class = \"main-item\" id=\"container-7\"><h1>" + correctlyAnswered + "</h1><br><h2>" + username + "\'s High Score</h2></div>";
+  let e = document.getElementById("displayResults");
+  e.innerHTML = "<div class = \"main-item\" id=\"container-7\"><h1>" + correctlyAnswered + "</h1><br><h2>" + username + "\'s Last Score</h2></div>";
 
 }
 
