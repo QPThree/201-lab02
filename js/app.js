8b556e9cf69fd2da272b474208d5a1f2d6bad981
QPThree <youngqp3@gmail.com>
2021-05-05 21:21:38 -0700

    refactored and made DRY

--- js/app.js
+++ js/app.js
@@ -5,9 +5,10 @@ let globalCorrect = 0;
 let answerBank = ['y',
   'yes',
   'n',
-  'no'];
+  'no',
+  23,
+  ['36', '38', '39', '49', '51', '53', '55']];
 let userName;
-
 
 //---functions declared----
 
@@ -18,7 +19,7 @@ function verifyName(name){
   }
   userName = name;
 }
-//question1
+//Main quiz (5 questions)
 function mainQuiz(){
   let answer = prompt('Is Quen from Maine?');
   answer = answer.toLowerCase();
@@ -98,26 +99,23 @@ function mainQuiz(){
   //console.log(userName + ', your score:', correct);
   alert(`${userName}, your score so far: ${globalCorrect}`);
 }
-
-
-// }
 //question 6
 //for loop track number of guesses
 //had to convert answer to int via parseint for the strictly equals 23 requirement
 function q6(){
   let wrong = 0;
-  let correctAge = 0;
+  let correctAge = false;
   let i = 1;
   alert('Next, you have 4 chances to guess what age I finished college!');
-  while (wrong !== 4 && correctAge === 0){
+  while (wrong !== 4 && correctAge === false){
     let answer = prompt(`Guess ${i}:`);
-    if (parseInt(answer) === 23){
+    if (parseInt(answer) === answerBank[4]){
       console.log('Correct');
       alert('Nicely Done!');
-      correctAge = 1;
+      correctAge = true;
       globalCorrect = globalCorrect + 1;
     }
-    else if(parseInt(answer) < 23){
+    else if(parseInt(answer) < answerBank[4]){
       wrong = wrong + 1;
       console.log('Wrong');
       alert(`Too low! You have ${4 - wrong} guesses remaining`);
@@ -138,12 +136,11 @@ function q6(){
 //array to store possible answers
 //for loops: 1. To count chances 2. To loop through the array, comparing the users guess with list of accepted answer
 function q7(){
-  let answerPossibilities = ['36', '38', '39', '49', '51', '53', '55'];
-  alert('Tom Brady Has won 7 Super Bowls.  Super Bowls are numbered from 1-55 (and counting).  Guess which Super Bowls Tom has won by entering integers between 1 and 55.  You have 6 guesses to get ONE correct. Go luck!');
+  alert('Tom Brady Has won 7 Super Bowls.  Super Bowls are numbered from 1-55 (and counting).  Guess which Super Bowls Tom has won by entering integers between 1 and 55.  You have 6 guesses to get ONE correct. Good luck!');
   for (let k = 0; k < 6; k++){
     let answer = prompt(`Guess ${k + 1}`);
-    for (let j = 0; j < answerPossibilities.length; j++){
-      if (answer === answerPossibilities[j]){
+    for (let j = 0; j < 7; j++){
+      if (answer === answerBank[5][j]){
         console.log('Correct Super Bowl Guessed');
         alert('Correct! Nicely done.');
         globalCorrect = globalCorrect + 1;
@@ -152,10 +149,9 @@ function q7(){
       }
     }
   }
-  alert(`Correct answers were: ${answerPossibilities}`);
+  alert(`Correct answers were: ${answerBank[5]}`);
   alert(`Your total correct out of 7 questions is: ${globalCorrect}`);
 }
-
 //---script runs in this order---
 alert('You are about to take a Quiz about me, Quen.  All questions are yes or no answers.  Please answer as such (yes / y or  no / n!');
 userName = prompt('Hello Please enter your name!');
@@ -163,80 +159,3 @@ verifyName(userName);
 mainQuiz();
 q6();
 q7();
-
-
-
-
-//reach code is below
-//may use in future iterations
-
-// //global vars
-// let username;
-// let correct;
-// //questions and related answers in banks must coordinate via index
-// let questionBank = [
-//   'Did Quen Grow up in Maine?',
-//   'Did Quen go to the University of New Hampshire?',
-//   'Is Quen planning to take the Javascript 401?',
-//   'Did Quen mention Snowboarding?',
-//   'Was Quen\'s major Kinesiology?'];
-// let answerBank = [
-//   'n',
-//   'y',
-//   'n',
-//   'n',
-//   'y'
-// ];
-
-// //gets username at document load
-// //user must submit a valid username.  No empty strings or canceling
-// function greeting(){
-//   username = prompt('Hello! Please enter a username');
-//   while(username === null || username === ''){
-//     username = prompt('Please enter a valid username!');
-//   }
-//   return 'Hello ' + username + '!';
-// }
-
-// //5 yes or no questions
-// //questions and answers come from  bank arrays
-// //answers are validated with while loop
-// //end of function will call displayResults function
-// //TODO: Change console.logs to alerts for user to see
-// function quizGame(){
-//   correct = 0;
-//   let answer;
-//   for (let i = 0; i < 5; i++){
-//     answer = prompt(questionBank[i]).toLowerCase();
-//     while (answer !== 'y' && answer !== 'yes' && answer !== 'no' && answer !== 'n'){
-//       answer = prompt('Please enter a valid respone');
-//     }
-//     if (answer.startsWith(answerBank[i])) {
-//       correct = correct + 1;
-//       console.log('Got question right');
-//       alert('That\'s Correct!');
-//     }
-//     else{
-//       console.log('Got question Wrong');
-//       alert('Unfortunately, no.');
-//     }
-//   }
-//   if (correct > 4){
-//     console.log('Smoked it!');
-//   }
-//   else if(correct > 2 && correct <= 4){
-//     console.log('Decent job ' + username + ' You answered ' + correct + ' correctly');
-//   }
-//   else{
-//     console.log('Yikes! Were you even paying attention?');
-//   }
-//   displayResults(correct);
-// }
-
-// //function gets element from html and writes the users score into it with HTML
-// function displayResults(correctlyAnswered){
-//   let e = document.getElementById("displayResults");
-//   e.innerHTML = "<div class = \"main-item\" id=\"container-7\"><h1>" + correctlyAnswered + "</h1><br><h2>" + username + "\'s Last Score</h2></div>";
-
-// }
-
